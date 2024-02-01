f4c235714e15beeb581b4dfbdb77107e6dd72fb5
Tasmplier322 <slavashishcov1@mail.ru>
2024-02-01 16:25:39 +0300

    Add function even or odd, also add block for displayinf a result into window DBMS_OUTPUT

--- LabsDB.sql
+++ LabsDB.sql
@@ -8,12 +8,42 @@ DECLARE
   v_val NUMBER;
 BEGIN
   FOR i IN 1..10000 LOOP
-    -- Генерация случайных значений для id и val
     v_id := ROUND(DBMS_RANDOM.VALUE(1, 10000));
     v_val := ROUND(DBMS_RANDOM.VALUE(1, 100));
 
-    -- Вставка записи в таблицу
     INSERT INTO MyTable (id, val) VALUES (v_id, v_val);
   END LOOP;
-  COMMIT; -- Фиксация изменений
+  COMMIT;
 END;
+
+CREATE OR REPLACE FUNCTION CheckEvenOdd
+RETURN VARCHAR2
+IS
+  v_even_count NUMBER := 0;
+  v_odd_count NUMBER := 0;
+BEGIN
+  FOR rec IN (SELECT val FROM MyTable)
+  LOOP
+    IF MOD(rec.val, 2) = 0 THEN
+      v_even_count := v_even_count + 1;
+    ELSE
+      v_odd_count := v_odd_count + 1;
+    END IF;
+  END LOOP;
+
+  IF v_even_count > v_odd_count THEN
+    RETURN 'TRUE';
+  ELSIF v_even_count < v_odd_count THEN
+    RETURN 'FALSE';
+  ELSE
+    RETURN 'EQUAL';
+  END IF;
+END;
+
+DECLARE
+  v_result VARCHAR2(10);
+BEGIN
+  v_result := CheckEvenOdd();
+  DBMS_OUTPUT.PUT_LINE('Result: ' || v_result);
+END;
+
